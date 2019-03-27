6efb438417ac3043426ce73ea71f69f9cf9aa680
ClemCornet <32069074+ClemCornet@users.noreply.github.com>
2019-03-27 12:00:44 +0100

    Update ControllerSpec.js

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -154,6 +154,13 @@ describe('controller', function () {
 
 	it('should highlight "All" filter by default', function () {
 		// TODO: write test
+		var todo = {id: 42, title: 'my todo', completed: true};
+		setUpModel([todo]);
+
+		subject.setView('');
+
+		expect(view.render).toHaveBeenCalledWith('setFilter', '');
+
 	});
 
 	it('should highlight "Active" filter when switching to active view', function () {
